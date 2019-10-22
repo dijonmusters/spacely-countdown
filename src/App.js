92e8e48e4f217007badeb15d97d3ab6b133cdbaa
Jon Meyers <jon.meyers@rea-group.com>
2019-10-22 14:11:33 +1100

    Implement timer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import logo from './logo.png';
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
+import differenceInSeconds from 'date-fns/differenceInSeconds';
 
-function App() {
+const Container = styled.div`
+  min-height: 100vh;
+  background: rgb(112, 56, 186);
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 7rem;
+  text-align: center;
+`;
+
+const Small = styled.p`
+  font-size: 1.5rem;
+`;
+
+const App = () => {
+  const startDate = new Date('2019-10-28T09:00:00');
+  const timeUntil = formatDistanceToNow(startDate);
+  const [seconds, setSeconds] = useState();
+  useEffect(() => {
+    const diffInSeconds = differenceInSeconds(startDate, new Date());
+    setSeconds(diffInSeconds);
+    setInterval(() => {
+      setSeconds(differenceInSeconds(startDate, new Date()));
+    }, 1000);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <div>
+        <img src={logo} alt="logo" />
+        <p>{timeUntil}</p>
+        <Small>{seconds}s</Small>
+      </div>
+    </Container>
   );
-}
+};
 
 export default App;
